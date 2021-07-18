aea725c6edaa4d12fa97648914c6df6e551289f9
LightAlykard <lightalykard@yandex.ru>
2021-07-18 21:16:41 +0300

    Подисправлены запросы

--- quares.sql
+++ quares.sql
@@ -2,7 +2,7 @@
 select user_name, score, money_sum, people_all from users ORDER BY score DESC;
 
 -- получить списки зданий пользователей по имени и с названием зданий и их координат
-SELECT user_name, build_name, xCoord,yCoord FROM users, bildings, maps WHERE maps.user_id = users.id_user and maps.bilding_id=bildings.id_bild;
+SELECT user_name, build_name, xCoord,yCoord FROM users, bildings, maps WHERE maps.user_id = users.id_user and maps.bilding_id=bildings.id_bild and user_name='tUser23';
 
 -- получить статистику пользователя по имени
 select user_name, score, money_sum, money_per_turn, people_all, people_per_turn, people_free,people_milit,people_med,people_manag,milit_sum_boost,med_sum_boost, manag_sum_boost from users where user_name = 'testuser';
